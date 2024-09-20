9c6ec046175dc4507176f65489c62d143c7cf21f
BuildTools <unconfigured@null.spigotmc.org>
2024-09-20 23:20:11 +0200

    updated

--- src/main/java/de/dulli/tutorialmod/TutorialMod.java
+++ src/main/java/de/dulli/tutorialmod/TutorialMod.java
@@ -1,5 +1,6 @@
 package de.dulli.tutorialmod;
 
+import de.dulli.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/de/dulli/tutorialmod/item/ModItems.java
@@ -0,0 +1,13 @@
+package de.dulli.tutorialmod.item;
+
+import de.dulli.tutorialmod.TutorialMod;
+
+public class ModItems {
+
+
+
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+    }
+}
